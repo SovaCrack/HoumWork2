042bbd28d3d57fff359d8769a53c857af9a92940
Elena <knif-fly@mail.ru>
2023-09-26 20:05:14 +0300

    Added file zadacha13

---
+++ zadacha13/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число: ");
+int znach2 = Convert.ToInt32(Console.ReadLine());
+int Length2 = znach2.ToString().Length;
+if(Length2 >= 3)
+{
+    while (znach2 > 999)
+    {
+        znach2 = znach2 / 10;
+    }
+    int result = znach2 % 10;
+    Console.WriteLine("Третье число - " + result);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
