6a17bad464c891122748e19d16ddcef0adf98a11
Elena <knif-fly@mail.ru>
2023-09-26 19:37:51 +0300

    Added zadacha10 third number

---
+++ zadacha10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число 
+// и на выходе показывает вторую цифру этого числа. 
+Console.WriteLine("Введите трехзначное число: ");
+int znach = Convert.ToInt32(Console.ReadLine());
+int Length = znach.ToString().Length;
+if(Length == 3)
+{
+    int result = (znach / 10) % 10;
+    Console.WriteLine("Второе число - "  + result);
+}
+else
+{
+    Console.WriteLine("Третьего числа нет");
+}
